b75ac1f799c43bb237a8ff29ff5d1dbab564854e
Ryan <49738342+gipsoong@users.noreply.github.com>
2024-03-17 03:43:27 +0800

    Create a reference for the square divs and name it 'square'. Add the class 'square' to the reference.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('div');
+const square = document.createElement('div');
+
+square.classList.add('square');
+
+function createGrid() {
+    for (i = 1; i < 16; i++) {
+
+    }
+}
