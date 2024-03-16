7ff1a466ef82c13babdeeefd79241ba38a8b0539
Ryan <49738342+gipsoong@users.noreply.github.com>
2024-03-17 04:03:16 +0800

    Modify the for loop to loop for 256 times (16x16), it was previously looping for only 16 times.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('div');
 
 
 function createGrid() {
-    for (i = 1; i <= 16; i++) {
+    for (i = 1; i <= 256; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
         container.appendChild(square);
