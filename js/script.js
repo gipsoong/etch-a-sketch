e92e7d5a3610792193c9f25d0852e6cc22ea5c48
Ryan <49738342+gipsoong@users.noreply.github.com>
2024-03-17 03:57:48 +0800

    Add 'defer' to script tag to make js file run only after the html is loaded. Create the 16x16 grid using a for loop and adding 'appendChild' for each loop.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('div');
 const square = document.createElement('div');
 
-square.classList.add('square');
+square.classList.add('grid-square');
+
 
 function createGrid() {
-    for (i = 1; i < 16; i++) {
-
+    for (i = 1; i <= 16; i++) {
+        container.appendChild(square);
+        console.log(i);
     }
 }
+
+createGrid();
