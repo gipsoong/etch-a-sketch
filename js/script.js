e67b87585c8fbe6acea2cd1b025e6f18116694e4
Ryan <49738342+gipsoong@users.noreply.github.com>
2024-03-17 23:38:53 +0800

    Create a reference to all the divs with the 'grid-square' class using querySelectorAll. Log the reference to view the array of the entire grid.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 const container = document.querySelector('div');
 
-
-
 function createGrid() {
     for (i = 1; i <= 256; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
         container.appendChild(square);
-        console.log(i);
+        // console.log(i);
     }
 }
 
 createGrid();
+
+function changeColor() {
+    console.log('hi');
+}
+
+let gridSquare = document.querySelectorAll('.grid-square');
+// gridSquare.addEventListener('mouseover', changeColor);
+console.log(gridSquare);
