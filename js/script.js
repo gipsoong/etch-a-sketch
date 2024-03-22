f39a25ab1566d5e9ab0c937c01d3c847e51273b4
Ryan <49738342+gipsoong@users.noreply.github.com>
2024-03-22 23:50:27 +0800

    Fixed parsePrompt logic, remove the multiplication of userInput (was using it previously to get the total number of squares).

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const container = document.getElementById('grid-container');
 let baseSide = 16;
 
 function createGrid(num) {
-    for (i = 1; i <= baseSide; i ++) {
+    for (i = 1; i <= num; i ++) {
         let row = document.createElement('div');
         row.classList.add('grid-row');
         container.appendChild(row);
         
-        for (x = 1; x <= baseSide; x++) {
+        for (x = 1; x <= num; x++) {
             let square = document.createElement('div');
             square.classList.add('grid-square');
             row.appendChild(square);
@@ -25,16 +25,15 @@ document.getElementById('grid-container').addEventListener('mouseover', e => {
 });
 
 function parsePrompt() {
-    let promptUserInput = prompt('Enter the amount of squares desired on each side of the new grid!');
+    let userInput = prompt('Enter the amount of squares desired on each side of the new grid!');
 
-    if (isNaN(promptUserInput)) {
+    if (isNaN(userInput)) {
         // console.log(promptUserInput);
         alert('Please enter a number below 100!');
         return parsePrompt();
     } else {
         // console.log(`Is valid, user input ${promptUserInput}`);
-        let userInput = promptUserInput * promptUserInput;
-        console.log(userInput);
+        // console.log(promptUserInput);
         return userInput;
     }
 }
@@ -43,5 +42,5 @@ document.querySelector('button').addEventListener('click', function() {
     let askForSize = parsePrompt();
     container.innerHTML = '';
     
-    createGrid(5);
+    createGrid(askForSize);
 })
